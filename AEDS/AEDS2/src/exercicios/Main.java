3bb593d2d311c550542f9ad6ede21043ede5e796
fonteBean <n9ckbean@gmail.com>
2025-10-21 17:51:23 -0300

    Add: Tempo

--- AEDS/AEDS2/src/exercicios/Main.java
+++ AEDS/AEDS2/src/exercicios/Main.java
@@ -2,50 +2,94 @@ package exercicios;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 import exercicios.models.Game;
 
 
 public class Main{
 
-
-
-    public static void criaHeap(ArrayList<Game> games, int n) {
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapsort(games, n, i);
-        }
-    }
-
-    public static void heapsort(ArrayList<Game> games, int n, int i) {
+    static long tempo;
+    static int comparacoes = 0;
+    
+    public static void criaHeap(ArrayList<Game> jogos, int tam, int i){
         int raiz = i;
-        int esq = 2 * i + 1;
-        int dir = 2 * i + 2;
-
-        if (esq < n && games.get(esq).getId() > games.get(raiz).getId()) {
-            raiz = esq;
+        int esq = 2*i+1;
+        int dir = 2*i+2;
+      
+        if (esq < tam) {
+            comparacoes++; 
+            if (jogos.get(esq).getId() > jogos.get(raiz).getId()){
+                raiz = esq;
+                comparacoes++; 
+            }
         }
 
-        if (dir < n && games.get(dir).getId() > games.get(raiz).getId()) {
-            raiz = dir;
-        }
+     
+        if (dir < tam) {
+            comparacoes++; 
+            if (jogos.get(dir).getId() > jogos.get(raiz).getId()){
+                raiz = dir;
+                comparacoes++; 
 
-        if (raiz != i) {
-            Game tmp = games.get(i);
-            games.set(i, games.get(raiz));
-            games.set(raiz, tmp);
-
-            heapsort(games, n, raiz);
+            }
         }
+        if (raiz != i){
+            comparacoes++; 
+            Game aux = jogos.get(i);
+            jogos.set(i, jogos.get(raiz));
+            jogos.set(raiz, aux);
+            criaHeap(jogos, tam, raiz);
+        }
+        return;
+    }
+    public static  void heapsort(ArrayList<Game> jogos)
+    {
+        int tam = jogos.size();
+        for (int i= tam/2-1; i >=0; i--){
+            comparacoes++; 
+            criaHeap(jogos,tam,i);
+        }
+        for (int j = tam-1;j>0;j--){
+            comparacoes++; 
+            Game aux = jogos.get(0);
+            jogos.set(0, jogos.get(j));
+            jogos.set(j, aux);
+            criaHeap(jogos,j,0);
+        }   
     }
     
-    public static Game buscarPorId(ArrayList<Game> jogos, int id) {
-        for (Game g : jogos) {
-            if (g.getId() == id) return g;
-        }
-        return null;
+
+   public static void escreveLog() {
+    String path = "AEDS/AEDS2/src/exercicios/estruturas/log.txt";
+
+    try (FileWriter escritor = new FileWriter(path, true)) { 
+        escritor.write("889080\t" + "Tempo de execução = " + "\tNumero de Comparacoes = " + comparacoes);
+    } catch (IOException e) {
+        System.err.println("Erro ao escrever o log: " + e.getMessage());
     }
+}
+
+    public static Game buscarPorId(ArrayList<Game> jogos, int id) {
+    int esq = 0;
+    int dir = jogos.size() - 1;
+    while (esq <= dir) {
+        int meio = esq + (dir - esq) / 2;
+        int meioID = jogos.get(meio).getId();
+        if (meioID == id) {
+            return jogos.get(meio);
+        } else if (meioID < id) {
+            esq = meio + 1;
+        } else {
+            dir = meio - 1;
+        }
+    }
+    return null;
+}
     public static  void main(String[] args) {
         ArrayList<Game> list = new ArrayList<Game>();
         String path = "AEDS/AEDS2/src/exercicios/games.csv";
@@ -59,6 +103,8 @@ public class Main{
                 list.add(game);
                 line = scanf.readLine();
             }
+            
+            
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
@@ -67,11 +113,22 @@ public class Main{
                 String line = scanf.nextLine();
                 while (!line.equals("FIM")){
                     int id = Integer.parseInt(line);
+                    long inicio = System.nanoTime();
+                    heapsort(list);
+                    long fim = System.nanoTime();
+                    tempo = fim - inicio;
                     Game g = buscarPorId(list,id);
-                    if (g !=null) g.imprimir();
+                    if (g !=null){
+                        g.imprimir();
+                        System.out.println("SIM");
+                    }else{
+                        System.out.println("NAO");
+                    }
                     line = scanf.nextLine();
+                    escreveLog();
                 }
             }
+            
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
